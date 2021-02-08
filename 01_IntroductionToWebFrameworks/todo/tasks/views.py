b35bbacbf183c049151a7243b14ff400a1e83580
Ilnur <ilnrzakirov@gmail.com>
2021-02-08 12:10:58 +0300

    task_1

--- 01_IntroductionToWebFrameworks/todo/tasks/views.py
+++ 01_IntroductionToWebFrameworks/todo/tasks/views.py
@@ -8,8 +8,12 @@ class ToDoView(View):
 
     def get(self, request, *args, **kwargs):
         l1 = ['Установить python', 'Установить django', 'Запустить сервер', 'Написать код', 'Порадоваться результату',
-              'Посмотреть что получилось', 'Порадоваться еще раз', 'Задать суперпользователя']
-        response = "<ul><li>" + random.choice(l1) + "</li><li>" + random.choice(l1) + '</li><li>' + random.choice(l1) + '</li><li>' + random.choice(l1) + '</li><li>' + random.choice(l1) + '</li></ul>'
+              'Посмотреть что получилось', 'Порадоваться еще раз', 'Задать суперпользователя', 'Ввести пароль администратора', 'Ну еще что нибул сделать']
+        response1 = random.sample(l1, 5)
+        response = "<ul>"
+        for i_team in response1:
+            response += "<li>" +  i_team + "</li>"
+        response +="</ul>"
 
 
         return HttpResponse(response)
