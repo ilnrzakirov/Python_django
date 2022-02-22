3b445f559ed40fc1ff2144845772f53aa982d1e2
bcarlee <bcarlee@student.21-school.ru>
2022-02-22 15:25:27 +0300

    add base

--- 09_Files/djfiles/djfiles/settings.py
+++ 09_Files/djfiles/djfiles/settings.py
@@ -114,6 +114,11 @@ USE_L10N = True
 
 USE_TZ = True
 
+LANGUAGES = [
+    ('ru', 'Русский'),
+    ('en', 'English')
+]
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
