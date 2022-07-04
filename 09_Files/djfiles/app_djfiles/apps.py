623374d9e04e24fd38500933018a5a3729cb3ed5
bcarlee <ilnrzakirov@gmail.om>
2022-07-04 12:16:35 +0300

    apps.py add lang

--- 09_Files/djfiles/app_djfiles/apps.py
+++ 09_Files/djfiles/app_djfiles/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class AppDjfilesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'app_djfiles'
+    verbose_name = _('Новости')
