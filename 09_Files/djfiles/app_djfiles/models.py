96c8f8c7c6e8aca36a374ddf98e0b68731caea44
bcarlee <ilnrzakirov@gmail.om>
2022-07-04 12:18:12 +0300

    models.py add lang

--- 09_Files/djfiles/app_djfiles/models.py
+++ 09_Files/djfiles/app_djfiles/models.py
@@ -2,17 +2,26 @@
 from django.db import models
 from django.contrib.auth.models import User, AbstractUser
 from django.core.validators import MinValueValidator, MaxValueValidator
+from django.utils.translation import gettext_lazy as _
 
 
 class Blog(models.Model):
-    name = models.CharField(max_length=1000, verbose_name="Название")
-    description = models.CharField(max_length=1000, verbose_name="Содрежание", default='')
-    created_at = models.DateTimeField(verbose_name="Дата создания", auto_now_add=True)
-    update_at = models.DateTimeField(verbose_name="Дата редактирования", auto_now=True)
+    name = models.CharField(max_length=1000, verbose_name=_("Название"))
+    description = models.CharField(max_length=1000, verbose_name=_("Содрежание"), default='')
+    created_at = models.DateTimeField(verbose_name=_("Дата создания"), auto_now_add=True)
+    update_at = models.DateTimeField(verbose_name=_("Дата редактирования"), auto_now=True)
+
+    class Meta:
+        verbose_name_plural = _('блог')
+        verbose_name = _('блог')
 
 class File(models.Model):
-    file = models.FileField(verbose_name="Изображение", default=None, upload_to='files/')
+    file = models.FileField(verbose_name=_("Изображение"), default=None, upload_to='files/')
     blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name_plural = _('файлы')
+        verbose_name = _('файл')
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -20,4 +29,8 @@ class Profile(models.Model):
     city = models.CharField(max_length=20, blank=True)
     verification = models.BooleanField(default=False)
     avatar = models.ImageField(default=None, upload_to='files/')
+
+    class Meta:
+        verbose_name_plural = _('профили')
+        verbose_name = _('профиль')
 
